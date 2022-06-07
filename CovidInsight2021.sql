4a8427bdd8e7e275cb411b0dee3426060c1ae10f
iLyass <103530153+ilyassbmdl@users.noreply.github.com>
2022-06-07 07:10:37 +0100

    Update CovidInsight2021.sql

--- CovidInsight2021.sql
+++ CovidInsight2021.sql
@@ -3,24 +3,28 @@ SELECT location, date, total_cases, new_cases, total_deaths, population
  order by location, date
 
 /* shows likelihood of dying if you contract covid in your country */
+
 SELECT location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 AS DeathPercentage
  FROM `server-181713.Covid.covidDeaths`
  Where location like '%States%'
  order by 2 ASC
 
 -- Looking at total Cases VS Population the percentage of population got covid
+
 SELECT location, date, total_cases, population, (total_cases/population)*100 AS InfectionPercentage
  FROM `server-181713.Covid.covidDeaths`
  Where location like '%States%'
  order by InfectionPercentage ASC
 
  --Looking at countries with highest Infection Rate compared to population
+ 
 SELECT location, population, Max(total_cases) AS HighestInfectionCount, Max((total_cases/population))*100 AS TopInfectionPercentage
  FROM `server-181713.Covid.covidDeaths`
 Group by location, population
 order by TopInfectionPercentage DESC
 
 --Showing countries with highest Death Count per population
+
 SELECT location, population, Max(total_deaths) AS HighestDeathCount
 FROM `server-181713.Covid.covidDeaths`
 where continent is  not null
@@ -28,6 +32,7 @@ Group by location, population
 order by HighestDeathCount DESC
 
 --Showing countries with highest Death Count per population
+
 SELECT continent,  Max(total_deaths) AS HighestDeathCount
 FROM `server-181713.Covid.covidDeaths`
 where continent is   not null
@@ -36,6 +41,7 @@ Order by HighestDeathCount DESC
 
 
 --Global numbers
+
 SELECT   SUM(total_deaths) AS Global_Deaths, SUM(total_cases) AS Global_cases, (SUM(total_deaths)/SUM(total_cases))*100 AS TopInfectionPercentage
 FROM `server-181713.Covid.covidDeaths`
 where continent is not null
@@ -45,6 +51,7 @@ where continent is not null
 
 --looking at Total Population vs Vaccinations
 -- USE CTE
+
 
 
 with PopVsvac (Continent, Location, Date, population, new_vaccinations, RollingPeopleVaccinated)
@@ -61,6 +68,8 @@ select *, (RollingPeopleVaccinated/population)*100 as RollingPeopleVaccinatedPer
 from PopVsvac
 
 --temp table
+
+
 Drop table if exists `PercentPopulationVaccinated`
 CREATE TABLE `PercentPopulationVaccinated` (
   'Continent' STRING,
